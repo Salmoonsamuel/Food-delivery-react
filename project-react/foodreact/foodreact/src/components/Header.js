0132b611bee9f0bbb28f5e03cd941e5f4d3b178c
Salmoonsamuel <salmoonsamuel08@gmail.co>
2021-05-26 01:59:20 -0400

    Changed the color and background color of header

--- project-react/foodreact/foodreact/src/components/Header.js
+++ project-react/foodreact/foodreact/src/components/Header.js
@@ -10,30 +10,33 @@ import {
   NavbarText
 } from 'reactstrap';
 
-const Example = (props) => {
+const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Ss-Foods</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
+      <Navbar className="bg-dark "  expand="md">
+        <NavbarBrand href="/" className="offset-1">Ss-Foods</NavbarBrand>
+        <NavbarToggler  onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="offset-4" navbar>
+          <Nav className="offset-3" navbar>
             <NavItem className="px-2" >
-              <NavLink href="/">Home</NavLink>
+              <NavLink href="/" className="text-light"><i class="fa fa-home"></i>Home</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="">About</NavLink>
+              <NavLink href="" className="text-light">About</NavLink>
+            </NavItem>
+            <NavItem>
+               <NavLink className="text-light">Contact</NavLink>
             </NavItem>
           </Nav>
-          <NavbarText>Contact</NavbarText>
+         
         </Collapse>
       </Navbar>
     </div>
   );
 }
 
-export default Example;
+export default Header;
